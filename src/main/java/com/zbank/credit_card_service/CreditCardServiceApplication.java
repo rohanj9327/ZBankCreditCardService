d85f13959c9fee1a25a728aa56a843d85ebda7ce
nandishbhuva01 <nandish.bhuva@exabitz.com>
2026-05-09 12:50:30 +0530

    Initial commit

---
+++ src/main/java/com/zbank/credit_card_service/CreditCardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.zbank.credit_card_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditCardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditCardServiceApplication.class, args);
+	}
+
+}
