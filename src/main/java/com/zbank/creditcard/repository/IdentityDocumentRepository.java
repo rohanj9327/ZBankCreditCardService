7ca7b98783a2535b0daf53272b8ce1c37d67e3d1
nandishbhuva01 <nandish.bhuva@exabitz.com>
2026-05-09 15:06:17 +0530

    remove unwanted data

--- src/main/java/com/zbank/creditcard/repository/IdentityDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.zbank.creditcard.repository;
-
-import com.zbank.creditcard.entity.IdentityDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IdentityDocumentRepository extends JpaRepository<IdentityDocument, Long> {
-}
