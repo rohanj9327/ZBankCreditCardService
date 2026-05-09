706f0523c536b710c153a6ec8df47690c97c26b2
nandishbhuva01 <nandish.bhuva@exabitz.com>
2026-05-09 15:10:14 +0530

    corrected packages

--- src/main/java/com/zbank/creditcard/contoller/CreditCardController.java
+++ src/main/java/com/zbank/creditcard/contoller/CreditCardController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/credit")
-public class CeditCardController {
+public class CreditCardController {
 
     private final CreditCardService creditCardService;
 
